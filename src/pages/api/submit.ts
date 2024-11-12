09f7d60fa552d031e9598dccdb0e195e0b55d4ab
Chandra Adipraja <chandracipicung@gmail.com>
2024-11-12 22:50:25 +0700

    fix bug message

--- src/pages/api/submit.ts
+++ src/pages/api/submit.ts
@@ -14,6 +14,16 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
 
     const body = req.body as sheetForm
 
+    const privateKey = process.env.GOOGLE_PRIVATE_KEY
+  ? process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  : null;
+
+if (!privateKey) {
+  console.error("GOOGLE_PRIVATE_KEY is not set in the environment variables.");
+  throw new Error("Environment variable GOOGLE_PRIVATE_KEY is required");
+}
+console.log("GOOGLE_PRIVATE_KEY:", process.env.GOOGLE_PRIVATE_KEY);
+
     try {
         const auth = new google.auth.GoogleAuth({
             credentials: {
